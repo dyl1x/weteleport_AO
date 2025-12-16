55de1a3d07c14d7bd90337931e6adcd60ad94188
Chama E <chamathme@gmail.com>
2025-12-16 22:42:38 +1100

    added pnd_mujoco files with the halfway done bvh converter, ugly code

---
+++ pnd_mujoco/example/python/bvh_test.py
@@ -0,0 +1,227 @@
+'''
+Docstring for pnd_mujoco.example.python.bvh_test
+'''
+import pdb
+import time
+import sys
+import numpy as np
+
+from bvhparser import * # pylint: disable=W0614,w0401
+
+from pndbotics_sdk_py.core.channel import ChannelPublisher, ChannelFactoryInitialize # type: ignore # pylint: disable=E0401
+from pndbotics_sdk_py.idl.default import adam_u_msg_dds__LowCmd_ # type: ignore  # pylint: disable=E0401
+from pndbotics_sdk_py.idl.default import adam_u_msg_dds__LowState_ # type: ignore  # pylint: disable=E0401,w0611
+from pndbotics_sdk_py.idl.adam_u.msg.dds_ import LowCmd_ # type: ignore  # pylint: disable=E0401
+from pndbotics_sdk_py.idl.adam_u.msg.dds_ import LowState_ # type: ignore # pylint: disable=E0401,w0611
+from pndbotics_sdk_py.idl.adam_u.msg.dds_ import HandCmd_ # type: ignore # pylint: disable=E0401
+from pndbotics_sdk_py.idl.default import adam_u_msg_dds__HandCmd_ # type: ignore # pylint: disable=E0401
+
+
+ADAM_U_NUM_MOTOR = 19
+KP_CONFIG = [
+    405.0,  # waistRoll (0)
+    405.0,  # waistPitch (1)
+    205.0,  # waistYaw (2)
+    9.0,   # neckYaw (3)
+    9.0,   # neckPitch (4)
+    180.0,  # shoulderPitch_Left (5)
+    180.0,   # shoulderRoll_Left (6)
+    9.0,   # shoulderYaw_Left (7)
+    9.0,   # elbow_Left (8)
+    9.0,   # wristYaw_Left (9)
+    9.0,   # wristPitch_Left (10)
+    9.0,   # wristRoll_Left (11)
+    180.0,  # shoulderPitch_Right (12)
+    180.0,   # shoulderRoll_Right (13)
+    9.0,   # shoulderYaw_Right (14)
+    9.0,   # elbow_Right (15)
+    9.0,   # wristYaw_Right (16)
+    9.0,   # wristPitch_Right (17)
+    9.0    # wristRoll_Right (18)
+]
+
+# Kd 配置数组（对应19个关节）
+KD_CONFIG = [
+    6.75,   # waistRoll (0)
+    6.75,   # waistPitch (1)
+    3.42,   # waistYaw (2)
+    0.9,   # neckYaw (3)
+    0.9,   # neckPitch (4)
+    1.8,   # shoulderPitch_Left (5)
+    1.8,   # shoulderRoll_Left (6)
+    0.9,   # shoulderYaw_Left (7)
+    0.9,   # elbow_Left (8)
+    0.9,   # wristYaw_Left (9)
+    0.9,   # wristPitch_Left (10)
+    0.9,   # wristRoll_Left (11)
+    1.8,   # shoulderPitch_Right (12)
+    1.8,   # shoulderRoll_Right (13)
+    0.9,   # shoulderYaw_Right (14)
+    0.9,   # elbow_Right (15)
+    0.9,   # wristYaw_Right (16)
+    0.9,   # wristPitch_Right (17)
+    0.9    # wristRoll_Right (18)
+]
+
+
+open_arm_pos = np.array([0, 0, 0,
+                       0.7, -0.5,
+        -1.6, 2.06, -1.65, -1.77,
+                      0.32, 0, 0,
+       - 1.6, -2.06, 1.65, -1.77,
+                      0.32, 0, 0
+],
+                              dtype=float)
+
+close_arm_pos = np.array([0, 0, 0,
+                             0, 0,
+                       0, 0, 0, 0,
+                          0, 0, 0,
+                       0, 0, 0, 0,
+                          0, 0, 0
+],
+                                dtype=float)
+
+open_hand = np.array([1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000], dtype=int)
+close_hand = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=int)
+
+dt = 0.0025
+runing_time = 0.0
+
+# input("Press enter to start")
+
+def importbvh():
+    '''
+    Docstring for importbvh
+    '''
+    # --- BVH → URDF mapping ---
+    bvh_to_urdf = {
+        'Spine':   ('waistYaw',   'Z'),
+        'Spine1':  ('waistPitch', 'Y'),
+        'Spine2':  ('waistRoll',  'X'),
+
+        'Neck':   ('neckYaw',   'Z'),
+        'Neck1':  ('neckPitch', 'Y'),
+
+        'LeftShoulder': ('shoulderYaw_Left',   'Z'),
+        'LeftArm':      ('shoulderPitch_Left', 'Y'),
+        'LeftForeArm':  ('elbow_Left',          'Y'),
+        'LeftHand':     ('wristPitch_Left',     'Y'),
+
+        'RightShoulder': ('shoulderYaw_Right',   'Z'),
+        'RightArm':      ('shoulderPitch_Right', 'Y'),
+        'RightForeArm':  ('elbow_Right',          'Y'),
+        'RightHand':     ('wristPitch_Right',     'Y'),
+    }
+
+
+    filename = 'pnd_mujoco/example/python/take001_chr01.bvh'
+    skeleton_data = ProcessBVH(filename)
+    joints = skeleton_data[0]
+    joints_offsets = skeleton_data[1]
+    joints_hierarchy = skeleton_data[2]
+    root_positions = skeleton_data[3]
+    joints_rotations = skeleton_data[4] #this contains the angles in degrees, size = num frames
+    joints_saved_angles = skeleton_data[5] #this contains channel information. E.g ['Xrotation', 'Yrotation', 'Zrotation']
+    joints_positions = skeleton_data[6]
+    joints_saved_positions = skeleton_data[7]
+
+
+    """
+    Number of frames skipped is controlled with this variable below. If you want all frames, set to 1.
+    """
+    frame_skips = 5
+
+    frames = []
+
+    for i in range(0,len(joints_rotations), frame_skips):
+
+        frame_data = joints_rotations[i]
+
+        frame_joints_rotations = {}
+
+        for joint in joints:
+            if joint in bvh_to_urdf:
+                frame_joints_rotations[joint] = []
+
+        #fill in the rotations dict
+        joint_index = 0
+        for joint in joints:
+            if joint in bvh_to_urdf:
+                frame_joints_rotations[joint] = frame_data[joint_index:joint_index+3]
+
+            joint_index += 3
+        frames.append(frame_joints_rotations)
+            
+    print('frame size: ', len(frames))
+    return frames
+
+
+if __name__ == '__main__':
+
+    importbvh()
+
+
+    if len(sys.argv) <2:
+        ChannelFactoryInitialize(1, "lo")
+    else:
+        ChannelFactoryInitialize(1, sys.argv[1])
+
+    # Create a publisher to publish the data defined in UserData class
+    pub = ChannelPublisher("rt/lowcmd", LowCmd_)
+    pub.Init()
+    cmd = adam_u_msg_dds__LowCmd_()
+
+    hand_pub = ChannelPublisher("rt/handcmd", HandCmd_)
+    hand_pub.Init()
+    hand_cmd = adam_u_msg_dds__HandCmd_()
+    #for i in range(19):
+     #   cmd.motor_cmd[i].q = 0.0
+      #  cmd.motor_cmd[i].kp = 0.0
+       # cmd.motor_cmd[i].dq = 0.0
+        #cmd.motor_cmd[i].kd = 0.0
+        #cmd.motor_cmd[i].tau = 0.0
+
+    while True:
+        step_start = time.perf_counter()
+
+        runing_time += dt
+
+        if (runing_time < 3.0):
+            # Stand up in first 3 second
+            
+            # Total time for standing up or standing down is about 1.2s
+            phase = np.tanh(runing_time / 1.2)
+            for i in range(ADAM_U_NUM_MOTOR):
+                cmd.motor_cmd[i].q = phase * open_arm_pos[i] + (
+                    1 - phase) * close_arm_pos[i]
+                # 使用配置的 Kp 和 Kd 值
+                cmd.motor_cmd[i].kp = KP_CONFIG[i]
+                cmd.motor_cmd[i].dq = 0.0
+                cmd.motor_cmd[i].kd = KD_CONFIG[i]
+                cmd.motor_cmd[i].tau = 0.0
+
+            for i in range(12):
+                hand_cmd.position[i] = close_hand[i]
+        else:
+            # Then stand down
+            phase = np.tanh((runing_time - 3.0) / 1.2)
+            for i in range(ADAM_U_NUM_MOTOR):
+                cmd.motor_cmd[i].q = phase * close_arm_pos[i] + (
+                    1 - phase) * open_arm_pos[i]
+                # 使用配置的 Kp 和 Kd 值
+                cmd.motor_cmd[i].kp = KP_CONFIG[i]
+                cmd.motor_cmd[i].dq = 0.0
+                cmd.motor_cmd[i].kd = KD_CONFIG[i]
+                cmd.motor_cmd[i].tau = 0.0
+
+            for i in range(12):
+                hand_cmd.position[i] = open_hand[i]
+
+        #print(cmd.motor_cmd[6].q)
+        pub.Write(cmd)
+        hand_pub.Write(hand_cmd)
+
+        time_until_next_step = dt - (time.perf_counter() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
---
+++ pnd_mujoco/simulate_python/pndbotics_sdk_py_bridge.py
@@ -0,0 +1,187 @@
+import mujoco
+import numpy as np
+import pygame
+import sys
+import struct
+
+from pndbotics_sdk_py.core.channel import ChannelSubscriber, ChannelPublisher
+
+from pndbotics_sdk_py.utils.thread import RecurrentThread
+
+from pndbotics_sdk_py.idl.adam_u.msg.dds_ import *
+from pndbotics_sdk_py.idl.default import adam_u_msg_dds__LowState_
+import config
+
+TOPIC_LOWCMD = "rt/lowcmd"
+TOPIC_LOWSTATE = "rt/lowstate"
+TOPIC_HAND_POSE = "rt/handcmd"
+
+NUM_MOTOR_IDL_ADAM_U = 19
+NUM_MOTOR_IDL_ADAM_LITE = 23
+NUM_MOTOR_IDL_ADAM_SP = 29
+
+class pndSdkBridge:
+
+    def __init__(self, mj_model, mj_data):
+        self.mj_model = mj_model
+        self.mj_data = mj_data
+
+        if(config.ROBOT != "adam_lite"):
+            self.num_motor = self.mj_model.nu - 24
+        else:
+            self.num_motor = self.mj_model.nu
+        self.dt = self.mj_model.opt.timestep
+        self.joystick = None
+
+        # PNDBotics SDK message
+        self.low_state = adam_u_msg_dds__LowState_()
+        self.low_state_puber = ChannelPublisher(TOPIC_LOWSTATE, LowState_)
+        self.low_state_puber.Init()
+        self.lowStateThread = RecurrentThread(
+            interval=self.dt, target=self.PublishLowState, name="sim_lowstate"
+        )
+        self.lowStateThread.Start()
+
+        # subscriber hand cmd_
+        self.hand_cmd_suber = ChannelSubscriber(TOPIC_HAND_POSE, HandCmd_)
+        self.hand_cmd_suber.Init(self.HandCmdHandler, 10)
+
+        self.low_cmd_suber = ChannelSubscriber(TOPIC_LOWCMD, LowCmd_)
+        self.low_cmd_suber.Init(self.LowCmdHandler, 10)
+
+    def LowCmdHandler(self, msg: LowCmd_):
+        if self.mj_data != None:
+            for i in range(self.num_motor):
+                self.mj_data.ctrl[i] = (
+                    msg.motor_cmd[i].tau
+                    + msg.motor_cmd[i].kp
+                    * (msg.motor_cmd[i].q - self.mj_data.sensordata[i])
+                    + msg.motor_cmd[i].kd
+                    * ( 
+                        msg.motor_cmd[i].dq
+                        - self.mj_data.sensordata[i + self.num_motor]
+                    )
+                )
+
+    def PublishLowState(self):
+        if self.mj_data != None:
+            for i in range(self.num_motor):
+                self.low_state.motor_state[i].q = self.mj_data.sensordata[i]
+                self.low_state.motor_state[i].dq = self.mj_data.sensordata[
+                    i + self.num_motor
+                ]
+                self.low_state.motor_state[i].tau_est = self.mj_data.sensordata[
+                    i + 2 * self.num_motor
+                ]
+            self.low_state_puber.Write(self.low_state)
+
+    def HandCmdHandler(self, msg: HandCmd_):
+        if self.mj_data != None:
+            fingers_pos = msg.position[0:12]
+            
+            # 创建 fingers 列表，每个 fingers_pos 的值重复两次
+            fingers = [finger for finger in fingers_pos for _ in range(2)]
+            
+            if config.HANDPOSE_SRC == 0:
+                for i in range(self.num_motor, self.num_motor + 24):
+                    self.mj_data.ctrl[i] = fingers[i - self.num_motor]
+            else:
+                # 修改 fingers 数组中的特定值
+                fingers[10] = fingers[8] * 0.5
+                fingers[8] = 2 * fingers[10]
+                fingers[9] = 2 * fingers[10]
+                fingers[22] = fingers[20] * 0.5
+                fingers[20] = 2 * fingers[22]
+                fingers[21] = 2 * fingers[22]
+                
+                for i in range(self.num_motor, self.num_motor + 24):
+                    self.mj_data.ctrl[i] = 1.6 - fingers[i - self.num_motor] * 0.0016
+                    
+                    if i == self.num_motor + 10:
+                        self.mj_data.ctrl[i] = 0.5 - fingers[i - self.num_motor] * 0.001
+                    
+                    if i in (self.num_motor + 11, self.num_motor + 9, self.num_motor + 8):
+                        self.mj_data.ctrl[i] = 1.0 - fingers[i - self.num_motor] * 0.001
+                    
+                    if i == self.num_motor + 22:
+                        self.mj_data.ctrl[i] = 0.5 - fingers[i - self.num_motor] * 0.001
+                    
+                    if i in (self.num_motor + 23, self.num_motor + 21, self.num_motor + 20):
+                        self.mj_data.ctrl[i] = 1.0 - fingers[i - self.num_motor] * 0.001
+
+
+    def PrintSceneInformation(self):
+        print(" ")
+
+        print("<<------------- Link ------------->> ")
+        for i in range(self.mj_model.nbody):
+            name = mujoco.mj_id2name(self.mj_model, mujoco._enums.mjtObj.mjOBJ_BODY, i)
+            if name:
+                print("link_index:", i, ", name:", name)
+        print(" ")
+
+        print("<<------------- Joint ------------->> ")
+        for i in range(self.mj_model.njnt):
+            name = mujoco.mj_id2name(self.mj_model, mujoco._enums.mjtObj.mjOBJ_JOINT, i)
+            if name:
+                print("joint_index:", i, ", name:", name)
+        print(" ")
+
+        print("<<------------- Actuator ------------->>")
+        for i in range(self.mj_model.nu):
+            name = mujoco.mj_id2name(
+                self.mj_model, mujoco._enums.mjtObj.mjOBJ_ACTUATOR, i
+            )
+            if name:
+                print("actuator_index:", i, ", name:", name)
+        print(" ")
+
+        print("<<------------- Sensor ------------->>")
+        index = 0
+        for i in range(self.mj_model.nsensor):
+            name = mujoco.mj_id2name(
+                self.mj_model, mujoco._enums.mjtObj.mjOBJ_SENSOR, i
+            )
+            if name:
+                print(
+                    "sensor_index:",
+                    index,
+                    ", name:",
+                    name,
+                    ", dim:",
+                    self.mj_model.sensor_dim[i],
+                )
+            index = index + self.mj_model.sensor_dim[i]
+        print(" ")
+
+
+class ElasticBand:
+
+    def __init__(self):
+        self.stiffness = 200
+        self.damping = 100
+        self.point = np.array([0, 0, 3.5])
+        self.length = 0
+        self.enable = True
+
+    def Advance(self, x, dx):
+        """
+        Args:
+          δx: desired position - current position
+          dx: current velocity
+        """
+        δx = self.point - x
+        distance = np.linalg.norm(δx)
+        direction = δx / distance
+        v = np.dot(dx, direction)
+        f = (self.stiffness * (distance - self.length) - self.damping * v) * direction
+        return f
+
+    def MujuocoKeyCallback(self, key):
+        glfw = mujoco.glfw.glfw
+        if key == glfw.KEY_7:
+            self.length -= 0.1
+        if key == glfw.KEY_8:
+            self.length += 0.1
+        if key == glfw.KEY_9:
+            self.enable = not self.enable
